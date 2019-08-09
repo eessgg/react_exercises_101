97b27ae7f9289799b9d64a04e1881583e674ea2b
Ester <estergomesdesign@gmail.com>
2019-08-09 10:52:48 -0300

    pokedex done

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Exercise 1: Slot Machine
 	- https://codesandbox.io/s/new-j23lx
 
+Exercise 2: Fruits
---
+++ react_pokedex/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+import Pokegame from './Pokegame'
+// import Pokedex from './Pokedex'
+
+function App () {
+  return (
+    <div className='App'>
+      <Pokegame />
+    </div>
+  )
+}
+
+export default App
